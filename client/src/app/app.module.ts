564fe492a659ad0514f0ecc2d252b1d86472cac7
@98Barakanus <baraka608@student.mmarau.ac.ke>
2023-12-10 01:45:54 +0300

    fixed navbar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { NavbarComponent } from './navbar/navbar.component';
 import { MinibarComponent } from './minibar/minibar.component';
 import { HomeComponent } from './home/home.component';
@@ -19,6 +19,7 @@ import { LandingComponent } from './landing/landing.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RegisterComponent } from './auth/register/register.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -38,10 +39,11 @@ import { RegisterComponent } from './auth/register/register.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule,
+
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
