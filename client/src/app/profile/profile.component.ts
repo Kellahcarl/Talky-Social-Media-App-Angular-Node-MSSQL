5ca97e1fff7d839c3a3d822a1015e834fe4bf3f9
@98Barakanus <baraka608@student.mmarau.ac.ke>
2023-12-10 18:35:23 +0300

    added edit profile

--- client/src/app/profile/profile.component.ts
+++ client/src/app/profile/profile.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, TemplateRef } from '@angular/core';
+import {
+  NgbDatepickerModule,
+  NgbOffcanvas,
+  OffcanvasDismissReasons,
+} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent {
+  private offcanvasService = inject(NgbOffcanvas);
+  closeResult = '';
 
+  open(content: TemplateRef<any>) {
+    this.offcanvasService.open(content, {
+      ariaLabelledBy: 'offcanvas-basic-title',
+    });
+  }
 }
